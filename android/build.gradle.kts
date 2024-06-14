bbe93836affdc7213017bf87ceb25db0926a2bdf
DreamingCodes <me@dreaming.codes>
2024-06-14 14:39:31 +0200

    Updated google billing library

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 }
 
 dependencies {
-    implementation("com.android.billingclient:billing-ktx:6.1.0")
+    implementation("com.android.billingclient:billing-ktx:7.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
